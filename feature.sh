3420bd165b6260bb54cbdb836596ef3b309ce484
Kaiserouo <chejps30221@gmail.com>
2022-12-29 17:10:36 +0800

    Final parameter

--- feature.sh
+++ feature.sh
@@ -1,21 +1,23 @@
+DATA_PATH="../hahow/data"
+
 python feature_extract.py \
   --feature_extract_task "eds" \
   --we_model_name_or_path "hfl/chinese-roberta-wwm-ext" \
-  --user_file "../hahow/data/users.csv" \
-  --item_file "../hahow/data/courses.csv" \
-  --chapter_file "../hahow/data/course_chapter_items.csv" \
-  --subgroup_file "../hahow/data/subgroups.csv" \
-  --train_item_file "../hahow/data/train.csv" \
+  --user_file "${DATA_PATH}/users.csv" \
+  --item_file "${DATA_PATH}/courses.csv" \
+  --chapter_file "${DATA_PATH}/course_chapter_items.csv" \
+  --subgroup_file "${DATA_PATH}/subgroups.csv" \
+  --train_item_file "${DATA_PATH}/train.csv" \
   --save_eds "./eds.hf" \
   --save_fdss "./fdss.hf"
 
 python feature_extract.py \
   --feature_extract_task "fdss" \
   --we_model_name_or_path "hfl/chinese-roberta-wwm-ext" \
-  --user_file "../hahow/data/users.csv" \
-  --item_file "../hahow/data/courses.csv" \
-  --chapter_file "../hahow/data/course_chapter_items.csv" \
-  --subgroup_file "../hahow/data/subgroups.csv" \
-  --train_item_file "../hahow/data/train.csv" \
+  --user_file "${DATA_PATH}/users.csv" \
+  --item_file "${DATA_PATH}/courses.csv" \
+  --chapter_file "${DATA_PATH}/course_chapter_items.csv" \
+  --subgroup_file "${DATA_PATH}/subgroups.csv" \
+  --train_item_file "${DATA_PATH}/train.csv" \
   --save_eds "./eds.hf" \
   --save_fdss "./fdss.hf"
--- pred.sh
+++ pred.sh
@@ -1,4 +1,4 @@
-MODEL_PATH="./models/def_w02_ep100"  # will put the result in this tt_model_dir as well
+MODEL_PATH="./models/def_w02_ep50"  # will put the result in this tt_model_dir as well
 # MODEL_PATH="${1}"  # will put the result in this tt_model_dir as well
 DATA_PATH="../hahow/data"
 
@@ -9,7 +9,8 @@ python two-towels.py \
   --load_tt_model_dir "${MODEL_PATH}" \
   --test_item_seen_file "../hahow/data/test_seen.csv" \
   --test_item_unseen_file "../hahow/data/test_unseen.csv" \
-  --save_rec_path "${MODEL_PATH}" 
+  --save_rec_path "${MODEL_PATH}" \
+  --tt_ns_w 0.2
 
 python ./generate_subgroup/seen_subgroup_baseline.py \
     --data_root "${DATA_PATH}" \
